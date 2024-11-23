77e760a694d907b955f3dcfda7ce15d82eff2ffb
Muhammad Rafa Fadlurahman <rafa.dasw@mail.ugm.ac.id>
2024-11-23 20:00:22 +0700

    add iklan

---
+++ src/components/freeDelivery/FreeDelivery.jsx
@@ -0,0 +1,12 @@
+import classes from './freeDelivery.module.css'
+import React from 'react'
+
+const FreeDelivery = () => {
+  return (
+    <div className={classes.freeDelivery}>
+      <span>Lord of The Rings Diskon 50%!</span>
+    </div>
+  )
+}
+
+export default FreeDelivery
