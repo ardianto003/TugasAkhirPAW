7a343f537afb7762626e221a8a6cd3a70413b147
Ardianto R <79231873+ardianto003@users.noreply.github.com>
2024-11-26 16:02:10 +0700

    Update route.js

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -3,14 +3,14 @@ import getCorsHeaders from "@/lib/apiCors";
 
 import Stripe from "stripe";
 
-// Initialize Stripe with your secret key
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request) {
   try {
     const { lineItems } = await request.json();
 
-    // Create a checkout session with Stripe
+    
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: lineItems,
@@ -19,7 +19,7 @@ export async function POST(request) {
       cancel_url: `${process.env.NEXTAUTH_URL}/cart`,
     });
 
-    // Return the session ID
+    
     return new Response(JSON.stringify({ id: session.id }), {
       status: 200,
       headers: {
@@ -29,7 +29,7 @@ export async function POST(request) {
   } catch (error) {
     console.error("Error creating checkout session:", error);
 
-    // Ensure a response is always returned
+    
     return new Response(JSON.stringify({ error: error.message }), {
       status: 400,
       headers: {
