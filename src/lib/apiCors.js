809da87cbaeee403d9f8dbdac4ee8b390e68fbc8
Ardianto R <79231873+ardianto003@users.noreply.github.com>
2024-11-26 16:06:08 +0700

    Update apiCors.js

--- src/lib/apiCors.js
+++ src/lib/apiCors.js
@@ -2,9 +2,9 @@ import { NextResponse, NextRequest } from "next/server";
 
 const getCorsHeaders = (origin) => {
   const headers = {
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS", // Allowing all common methods
-    "Access-Control-Allow-Headers": "*", // Allowing any headers
-    "Access-Control-Allow-Origin": "*", // Allowing any origin
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS", 
+    "Access-Control-Allow-Headers": "*", 
+    "Access-Control-Allow-Origin": "*", 
   };
 
   return headers;
